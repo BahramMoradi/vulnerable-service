ba63c071656079016a5c9bbd58cc9b423a1919de
Bahram Moradi <bahram.moradi@ufst.dk>
2020-11-26 16:08:11 +0100

    Minor changes

--- build-and-deploy.cmd
+++ build-and-deploy.cmd
@@ -1,6 +1,4 @@
 call gradle clean build
-call docker build . -t gateway
-REM call docker run -p 8080:8080 -i -t gateway:latest
-REM call docker-compose up
-call kubectl apply -f scripts/k8/gateway-deployment.yaml
-call kubectl apply -f scripts/k8/gateway-service.yaml
+call docker build . -t vulnerable
+call kubectl apply -f scripts/k8/vulnerable-deployment.yaml
+call kubectl apply -f scripts/k8/vulnerable-service.yaml
